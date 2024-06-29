caa16554a0841d56060d2849fc2923a2508b4a9b
Dilkhush Raj <dilkhush_raj@outlook.com>
2024-06-29 13:06:15 +0530

    update comment and tag model

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,9 +1,12 @@
 import mongoose, {Schema} from "mongoose";
 
-const commentSchema = new Schema({
-  user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
-  data: {type: String, required: true},
-  created_at: {type: Date, default: Date.now},
-});
+const commentSchema = new Schema(
+  {
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    data: {type: String, required: true},
+    created_at: {type: Date, default: Date.now},
+  },
+  {timestamps: true}
+);
 
 export const Comment = mongoose.model("Comment", commentSchema);
--- src/models/tag.model.js
+++ src/models/tag.model.js
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
 
-const TagSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const TagSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {timestamps: true}
+);
 
 export default mongoose.model("Tag", TagSchema);
